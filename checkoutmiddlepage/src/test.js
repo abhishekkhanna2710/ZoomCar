91cf2446f140ee8d0d93d9bb599dd037d394eff6
ravindernandal366 <ravindernandal366@gmail.com>
2023-01-08 21:19:35 +0530

    hello

---
+++ checkoutmiddlepage/src/test.js
@@ -0,0 +1,23 @@
+#dropCircleDotDiv{
+    width:20px;
+    height: 20px;
+}
+
+#dropDetailsDiv{
+    display:flex;
+    align-items: center;
+}
+
+#dropTimeAndPlaceDiv{
+    text-align: left;
+}
+
+#dropDataAndTimeH4Tag{
+    margin-top: 0px;
+    margin-bottom: 0px;
+}
+
+#dropPlacePTag{
+    margin-top: 0px;
+    margin-bottom: 0px;
+}
